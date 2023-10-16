b46b5ace5e7eea2e351a9037c8adc7a1bb4345f2
Lucía <78818963+lucia-ferrer@users.noreply.github.com>
2023-10-16 17:12:54 +0200

    Update base.py

--- base.py
+++ base.py
@@ -1,13 +1,9 @@
+import numpy as np
 import random
+import time
 
-# Definición de la instancia del problema TSP
-cities = {
-    "A": (0, 0),
-    "B": (2, 4),
-    "C": (5, 2),
-    "D": (7, 6),
-    "E": (3, 8)
-}
+# Cargar las coordenadas de las ciudades desde el archivo .npy
+cities = np.load('ciudades.npy')
 
 # Parámetros del algoritmo genético
 population_size = 100
@@ -16,31 +12,27 @@ mutation_rate = 0.1
 
 # Función para calcular la distancia entre dos ciudades
 def distance(city1, city2):
-    x1, y1 = city1
-    x2, y2 = city2
-    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+    return np.linalg.norm(city1 - city2)
 
 # Función para calcular la longitud del recorrido (fitness)
 def total_distance(route):
-    return sum(distance(cities[route[i]], cities[route[i + 1]]) for i in range(len(route) - 1))
+    return np.sum(distance(cities[route], np.roll(cities[route], shift=-1, axis=0), axis=1))
 
 # Genera una población inicial aleatoria
 def generate_population(size):
-    return [random.sample(cities.keys(), len(cities)) for _ in range(size)]
+    return np.array([np.random.permutation(len(cities)) for _ in range(size)])
 
 # Función de selección de padres (ruleta)
 def select_parents(population):
-    fitness_values = [1 / total_distance(individual) for individual in population]
-    total_fitness = sum(fitness_values)
-    probabilities = [fitness / total_fitness for fitness in fitness_values]
-    return random.choices(population, probabilities, k=2)
+    fitness_values = 1 / np.array([total_distance(individual) for individual in population])
+    probabilities = fitness_values / fitness_values.sum()
+    return np.array(random.choices(population, probabilities, k=2))
 
 # Función de cruce (orden)
 def crossover(parent1, parent2):
     start, end = sorted(random.sample(range(len(parent1)), 2))
-    child = [None] * len(parent1)
-    for i in range(start, end + 1):
-        child[i] = parent1[i]
+    child = np.array([None] * len(parent1))
+    child[start:end+1] = parent1[start:end+1]
     remaining_cities = [city for city in parent2 if city not in child]
     j = 0
     for i in range(len(parent1)):
@@ -55,6 +47,8 @@ def mutate(route):
     route[index1], route[index2] = route[index2], route[index1]
 
 # Algoritmo genético
+start_time = time.time()
+
 population = generate_population(population_size)
 for generation in range(generations):
     population = sorted(population, key=total_distance)
@@ -69,5 +63,16 @@ for generation in range(generations):
 
 best_route = population[0]
 best_distance = total_distance(best_route)
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+
+# Guardar la solución en un archivo de texto
+with open('ruta_optima.txt', 'w') as file:
+    file.write("Mejor recorrido: " + " -> ".join(map(str, best_route)))
+    file.write("\nDistancia del mejor recorrido: " + str(best_distance))
+    file.write(f"\nTiempo empleado: {elapsed_time:.4f} segundos")
+
 print("Mejor recorrido:", best_route)
 print("Distancia del mejor recorrido:", best_distance)
+print(f"Tiempo empleado: {elapsed_time:.4f} segundos")
